0a86de1cfa100a920b410b1cf296180b883d32b4
Nikita Tikhonov <jagarnikita@gmail.com>
2023-10-05 13:55:35 +0300

    Add error message above the button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,44 +1,57 @@
 // react native button component
 import React from 'react'
-import {TouchableOpacity, StyleSheet, Text} from 'react-native'
+import {TouchableOpacity, StyleSheet, Text, View} from 'react-native'
 import {colors} from '../../theme'
 
 type Props = {
   title: string
   onPress: () => void
   disabled?: boolean
+  errorMessage?: string
 }
 
-const _Button: React.FC<Props> = ({title, onPress, disabled}) => {
+const _Button: React.FC<Props> = ({title, onPress, disabled, errorMessage}) => {
   const backgroundColor = disabled ? colors.WARM_GREY : colors.PALE_TEAL
   return (
-    <TouchableOpacity
-      style={[styles.button, {backgroundColor}]}
-      onPress={onPress}
-      disabled={disabled}>
-      <Text style={styles.text}>{title}</Text>
-    </TouchableOpacity>
+    <View style={styles.buttonContainer}>
+      {errorMessage && <Text style={styles.error}>{errorMessage}</Text>}
+      <TouchableOpacity
+        style={[styles.button, {backgroundColor}]}
+        onPress={onPress}
+        disabled={disabled}>
+        <Text style={styles.text}>{title}</Text>
+      </TouchableOpacity>
+    </View>
   )
 }
 
 export const Button = React.memo(_Button)
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   button: {
     backgroundColor: colors.PALE_TEAL,
     borderRadius: 8,
     padding: 10,
     margin: 10,
-    width: '80%',
     height: 50,
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
   },
   text: {
     color: colors.WHITE,
     fontSize: 24,
     fontWeight: '400',
     textAlign: 'center',
+  },
+  error: {
+    color: colors.BUBBLE_GUM,
+    fontSize: 14,
   },
 })
 
