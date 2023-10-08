90ef9760f2150700215f045c23544ed7f1678b67
Nikita Tikhonov <jagarnikita@gmail.com>
2023-10-08 13:50:24 +0300

    Update layout

--- src/screens/SignInScreen/SignInScreen.tsx
+++ src/screens/SignInScreen/SignInScreen.tsx
@@ -114,6 +114,7 @@ const styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'flex-start',
     alignItems: 'center',
+    marginTop: '-5%',
   },
   title: {
     textAlign: 'center',
